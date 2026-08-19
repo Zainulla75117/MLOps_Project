1839fb52c9d9a22a49554cca51368e846b681ce4
Zainulla75117 <pzainullabideen@gmail.com>
2026-08-20 02:42:03 +0530

    Fix terraform S3 lifecycle configuration warning

--- Bengaluru Traffic Analysis/terraform/modules/s3/main.tf
+++ Bengaluru Traffic Analysis/terraform/modules/s3/main.tf
@@ -46,6 +46,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "mlflow_artifacts" {
     id     = "transition-old-artifacts"
     status = "Enabled"
 
+    filter {}
+
     transition {
       days          = 90
       storage_class = "STANDARD_IA"
